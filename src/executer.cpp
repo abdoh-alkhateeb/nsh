32aec2586f63072cced59e57ba5c9a3316bbacef
Omar Soliman <omarahmkamal007@gmail.com>
2026-03-01 22:08:32 +0200

    Added > operation

--- src/executer.cpp
+++ src/executer.cpp
@@ -4,15 +4,35 @@
 #include "sys/wait.h"
 #include <iostream>
 #include <vector>
+#include <fcntl.h>
+#include <algorithm>
 
 void Executer::execute(const std::vector<std::string> &tokens)
 {
     if (Builtins::handle(tokens))
         return;
+    std::vector<std::string> newTokens = tokens;
+    std::string outputFile;
+    bool redirect = false;
 
+    for (size_t i = 0; i < newTokens.size(); ++i)
+    {
+       if (newTokens[i] == ">")
+       {
+          if (i + 1 < newTokens.size())
+          {
+            redirect = true;
+            outputFile = newTokens[i + 1];
+
+            newTokens.erase(newTokens.begin() + i,
+                            newTokens.begin() + i + 2);
+          }
+          break;
+       }
+    }
     std::vector<const char *> argv;
 
-    for (const std::string &token : tokens)
+    for (const std::string &token : newTokens)
         argv.push_back(token.c_str());
     argv.push_back(nullptr);
 
@@ -20,7 +40,23 @@ void Executer::execute(const std::vector<std::string> &tokens)
 
     if (pid == 0)
     {
-        int status = execvp(argv[0], const_cast<char *const *>(argv.data()));
+        if (redirect)
+        {
+           int fd = open(outputFile.c_str(),
+                         O_CREAT | O_WRONLY | O_TRUNC,
+                         0644);
+
+         if (fd < 0)
+         {
+            std::cerr << "Failed to open file" << std::endl;
+            exit(1);
+         }
+
+         dup2(fd, STDOUT_FILENO);
+         close(fd);
+    }
+
+int status = execvp(argv[0], const_cast<char *const *>(argv.data()));
 
         if (status != 0)
         {
