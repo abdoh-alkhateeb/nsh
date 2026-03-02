785a82287999b84abbae7a3f81d6a9fa8a03a0a3
Christine Selwanes <christineselwanes@aucegypt.edu>
2026-03-02 11:52:48 +0000

    Support background processes with &

--- src/executer.cpp
+++ src/executer.cpp
@@ -9,44 +9,43 @@
 #include <vector>
 #include <string>
 #include <cerrno>
-#include <cstring>
 
 void Executer::execute(const std::vector<std::string> &tokens)
 {
-
     if (Builtins::handle(tokens))
         return;
 
-   
     bool redirect = false;
     std::string outFile;
+    bool background = false;
 
-    
     std::vector<std::string> cleanedTokens;
     cleanedTokens.reserve(tokens.size());
 
     for (size_t i = 0; i < tokens.size(); ++i) {
+        if (tokens[i] == "&" && i == tokens.size() - 1) {
+            background = true;
+            continue;
+        }
+
         if (tokens[i] == ">") {
-           
             if (i + 1 < tokens.size()) {
                 redirect = true;
                 outFile = tokens[i + 1];
-                ++i; 
+                ++i;
                 continue;
             } else {
                 std::cerr << "nsh: syntax error near unexpected token `>'\n";
                 return;
             }
         }
+
         cleanedTokens.push_back(tokens[i]);
     }
 
-  
-    if (cleanedTokens.empty()) {
+    if (cleanedTokens.empty())
         return;
-    }
 
-    
     std::vector<const char *> argv;
     argv.reserve(cleanedTokens.size() + 1);
 
@@ -58,9 +57,6 @@ void Executer::execute(const std::vector<std::string> &tokens)
 
     if (pid == 0)
     {
-        
-
-        
         if (redirect) {
             int fd = open(outFile.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
             if (fd < 0) {
@@ -79,7 +75,6 @@ void Executer::execute(const std::vector<std::string> &tokens)
 
         int status = execvp(argv[0], const_cast<char *const *>(argv.data()));
 
-        
         if (status != 0)
         {
             std::string msg = "failed to execute command";
@@ -93,12 +88,11 @@ void Executer::execute(const std::vector<std::string> &tokens)
     }
     else if (pid == -1)
     {
-        
         std::cerr << cleanedTokens[0] << ": failed to execute command" << std::endl;
     }
     else
     {
-        
-        waitpid(pid, nullptr, 0);
+        if (!background)
+            waitpid(pid, nullptr, 0);
     }
 }
