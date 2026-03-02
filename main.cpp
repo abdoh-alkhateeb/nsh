f01a897fca87525e25f61867d8ca2b85773da71c
Rola Elreedy <rolaelreedy@aycegypt.edu>
2026-03-02 14:23:57 +0200

    Implement output redirection using > operator

--- main.cpp
+++ main.cpp
@@ -1,7 +1,61 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main() {
-    std::cout << "Welcome to MyShell!" << std::endl;
+    std::string line;
+    std::vector<std::string> history;
+
+    while (true) {
+        std::cout << "$ ";
+        std::getline(std::cin, line);
+        if (line.empty()) continue;
+        history.push_back(line);
+
+        // Check for exit
+        if (line == "exit") break;
+
+        // Output redirection
+        std::string cmd = line;
+        std::string outfile;
+        size_t pos = line.find(">");
+        if (pos != std::string::npos) {
+            cmd = line.substr(0, pos);
+            outfile = line.substr(pos + 1);
+            outfile.erase(0, outfile.find_first_not_of(" "));
+        }
+
+        // Tokenize command
+        std::istringstream iss(cmd);
+        std::vector<char*> args;
+        std::string token;
+        while (iss >> token) {
+            char* arg = new char[token.size() + 1];
+            std::strcpy(arg, token.c_str());
+            args.push_back(arg);
+        }
+        args.push_back(nullptr);
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            if (!outfile.empty()) {
+                freopen(outfile.c_str(), "w", stdout);
+            }
+            execvp(args[0], args.data());
+            perror("execvp failed");
+            exit(1);
+        } else {
+            wait(nullptr);
+        }
+
+        // Free memory
+        for (auto arg : args) delete[] arg;
+    }
+
     return 0;
 }
 
