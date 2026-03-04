ec541af6b20c771f39e425541ad3099cf496dc6a
Youssef Sameh <Youssefsameh2778@gmail.com>
2026-03-04 15:38:34 +0200

    history implemented

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,16 @@ int main()
         std::string input;
         std::getline(std::cin, input);
         history.push_back(input);
+        
         if (input.empty())
             continue;
 
-    if (input == "history")
-    {
-        for (int i = 0; i < history.size(); ++i)
-            std::cout << i + 1 << " " << history[i] << std::endl;
-        continue;
-    }
+        if (input == "history")
+        {
+            for (int i = 0; i < history.size(); ++i)
+                std::cout << i + 1 << " " << history[i] << std::endl;
+            continue;
+        }
 
         std::vector<std::string> tokens = Parser::parseInput(input);
         Executer::execute(tokens);
