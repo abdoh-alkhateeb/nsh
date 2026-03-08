0a071624eb46c2508c69dc8db4032a67d2358380
malak <malakhosny101@gmail.com>
2026-03-08 23:15:18 +0200

    implement requirments

--- src/executer.cpp
+++ src/executer.cpp
@@ -4,16 +4,26 @@
 #include "sys/wait.h"
 #include <iostream>
 #include <vector>
-
+#include <fcntl.h>
 void Executer::execute(const std::vector<std::string> &tokens)
 {
     if (Builtins::handle(tokens))
         return;
 
     std::vector<const char *> argv;
-
+bool gt_exists=false;
+bool amp_exists=false;
     for (const std::string &token : tokens)
-        argv.push_back(token.c_str());
+{if(token=="&"){
+amp_exists=true;
+break;}
+if(token==">"){
+gt_exists=true;
+break;}
+  argv.push_back(token.c_str());
+
+}
+
     argv.push_back(nullptr);
 
     pid_t pid = fork();
@@ -22,7 +32,12 @@ void Executer::execute(const std::vector<std::string> &tokens)
         std::cerr << tokens[0] << ": failed to execute command" << std::endl;
     else if (pid == 0) // child process
     {
-        int status = execvp(argv[0], const_cast<char *const *>(argv.data()));
+if(gt_exists){	
+int fd = open(tokens[2].c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644); 
+dup2(fd, STDOUT_FILENO);
+close(fd);
+}
+   int status = execvp(argv[0], const_cast<char *const *>(argv.data()));
 
         if (status != 0)
         {
@@ -34,6 +49,6 @@ void Executer::execute(const std::vector<std::string> &tokens)
             std::cerr << tokens[0] << ": " << msg << std::endl;
         }
     }
-    else // parent process (pid > 0)
-        waitpid(pid, nullptr, 0);
-}
+    else { // parent process (pid > 0)
+      if(!amp_exists)  waitpid(pid, nullptr, 0);
+}}
