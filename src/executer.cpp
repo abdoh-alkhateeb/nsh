535d4153a883cc1663dc683e0f9540a9b3f845f0
Zeina <zeinaswiedan13@gmail.com>
2026-03-08 01:12:17 +0000

    Implemented the > operator successfuly

--- src/executer.cpp
+++ src/executer.cpp
@@ -4,8 +4,12 @@
 #include "sys/wait.h"
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <iostream>
+#include <fcntl.h>
 
-void Executer::execute(const std::vector<std::string> &tokens)
+
+void Executer::execute (std::vector<std::string> &tokens)
 {
     if (Builtins::handle(tokens))
         return;
@@ -19,7 +23,37 @@ void Executer::execute(const std::vector<std::string> &tokens)
     pid_t pid = fork();
 
     if (pid == 0)
-    {
+    { 
+	for(size_t i = 0; i < tokens.size(); i++) {
+
+		if (tokens[i] == ">") {
+
+		int fd = open(tokens[i+1].c_str(), O_WRONLY | O_CREAT | O_TRUNC , 0644);
+
+			dup2(fd , STDOUT_FILENO);
+
+			close(fd);
+
+			tokens.erase(tokens.begin() + i, tokens.begin() + i + 2);
+ 
+			break; 
+
+		}
+
+	}
+
+
+
+	std::vector<const char*> argv;
+
+	for (const std::string &token : tokens) {
+
+		argv.push_back(token.c_str());  }
+
+		argv.push_back(nullptr);
+
+
+
         int status = execvp(argv[0], const_cast<char *const *>(argv.data()));
 
         if (status != 0)
