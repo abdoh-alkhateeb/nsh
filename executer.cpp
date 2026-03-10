18fc83421a9cff6ffdf4ab640b977a228d218f06
Rola Kamel <Rkamel@aucegypt.edu>
2026-03-10 17:33:42 +0000

    done

---
+++ executer.cpp
@@ -0,0 +1,42 @@
+#include "executer.hpp"
+#include "bultins.hpp"
+#include "unistd.h"
+#include "sys/wait.h"
+#include <fcntl.h>
+#include <iostream>
+#include <vector>
+
+void Executer::execute(const std::vector<std::string> &tokens)
+{
+	if(Builtins::handle(tokens))
+		return;
+
+	std::vector<const char *>argv;
+	std::string outfile;
+
+	for(size_t i=0; i<tokens.size(); i++){
+		if(tokens[i] ==">")
+		{
+			outfile= tokens[i+1];
+			break;}
+		argv.push_back(tokens[i].c_str());}
+	argv.push_back(nullptr);
+
+	pid_t pid=fork();
+	if(pid<0)
+		std::cerr<<tokens[0]<<":failed to execute command"<<std::endl;
+	else if(pid ==0){
+	if(!outfile.empty()){
+		int fd=open("output.txt"), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		dup2(fd, STDOUT_FILENO);}
+
+	int status= execvp(argv[0], const_cast<char *const *>(argv.data()));
+
+	if(status !=0){
+		std::string msg = "failed to execute command";
+		if(errno == ENOENT)
+			msg="command not found";
+		std::cerr<<tokens[0]<<": "<<msg<<std::endl;}}
+
+		else
+			waitpid(pid, nullptr, 0);}
