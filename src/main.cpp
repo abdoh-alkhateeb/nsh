263b949a22129379f5f33c1fe79447504cd35ac2
Mostafa Amin <mostafaamin@Mostafas-MacBook-Air.local>
2026-03-10 20:26:45 +0200

    Implement history built-in command

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include "parser.hpp"
 #include "executer.hpp"
 #include <iostream>
+#include <vector>
+#include <string>
 
 int main()
 {
+    std::vector<std::string> historyList;
+
     while (true)
     {
         std::cout << "nsh> ";
@@ -12,6 +16,15 @@ int main()
 
         if (input.empty())
             continue;
+
+        historyList.push_back(input);
+        if (input == "history")
+        {
+            for (size_t i = 0; i < historyList.size(); ++i)
+                std::cout << i + 1 << " " << historyList[i] << std::endl;
+
+            continue;
+        }
 
         std::vector<std::string> tokens = Parser::parseInput(input);
         Executer::execute(tokens);
