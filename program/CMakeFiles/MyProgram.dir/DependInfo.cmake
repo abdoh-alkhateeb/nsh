d35d6246d11592b4f90c464547cc31e1b82f41d0
Omar Mostafa <omarmostafa082007@gmail.com>
2026-03-10 17:59:16 +0200

    Merged src and Include into one directory with CMakeLists.txt file added

---
+++ program/CMakeFiles/MyProgram.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/yassineahmedmostafa/fork1/fork_nsh/program/builtins.cpp" "CMakeFiles/MyProgram.dir/builtins.cpp.o" "gcc" "CMakeFiles/MyProgram.dir/builtins.cpp.o.d"
+  "/Users/yassineahmedmostafa/fork1/fork_nsh/program/executer.cpp" "CMakeFiles/MyProgram.dir/executer.cpp.o" "gcc" "CMakeFiles/MyProgram.dir/executer.cpp.o.d"
+  "/Users/yassineahmedmostafa/fork1/fork_nsh/program/main.cpp" "CMakeFiles/MyProgram.dir/main.cpp.o" "gcc" "CMakeFiles/MyProgram.dir/main.cpp.o.d"
+  "/Users/yassineahmedmostafa/fork1/fork_nsh/program/parser.cpp" "CMakeFiles/MyProgram.dir/parser.cpp.o" "gcc" "CMakeFiles/MyProgram.dir/parser.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
