f452faa3512c409db883fbdefefb8ee5607ce436
ibrahim <ibrahim.talal@aucegypt.edu>
2026-03-10 23:48:44 +0200

    >

--- src/executer.cpp
+++ src/executer.cpp
@@ -2,38 +2,85 @@
 #include "builtins.hpp"
 #include "unistd.h"
 #include "sys/wait.h"
+#include "fcntl.h"
 #include <iostream>
 #include <vector>
 
-void Executer::execute(const std::vector<std::string> &tokens)
+void Executer::execute(std::vector<std::string> tokens)
 {
+    if (tokens.empty())
+        return;
+
     if (Builtins::handle(tokens))
         return;
+
+    bool background = false;
+
+    if (tokens.back() == "&")
+    {
+        background = true;
+        tokens.pop_back();
+    }
+
+    bool redirect = false;
+    std::string outfile;
+
+    for (size_t i = 0; i < tokens.size(); i++)
+    {
+        if (tokens[i] == ">" && i + 1 < tokens.size())
+        {
+            redirect = true;
+            outfile = tokens[i + 1];
+
+            tokens.erase(tokens.begin() + i, tokens.begin() + i + 2);
+            break;
+        }
+    }
 
     std::vector<const char *> argv;
 
     for (const std::string &token : tokens)
         argv.push_back(token.c_str());
+
     argv.push_back(nullptr);
 
     pid_t pid = fork();
 
-    if (pid < 0) // fork failed
+    if (pid < 0)
         std::cerr << tokens[0] << ": failed to execute command" << std::endl;
-    else if (pid == 0) // child process
+
+    else if (pid == 0)
     {
-        int status = execvp(argv[0], const_cast<char *const *>(argv.data()));
-
-        if (status != 0)
+        if (redirect)
         {
-            std::string msg = "failed to execute command";
+            int fd = open(outfile.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
-            if (errno == ENOENT)
-                msg = "command not found";
+            if (fd < 0)
+            {
+                perror("open");
+                exit(1);
+            }
 
-            std::cerr << tokens[0] << ": " << msg << std::endl;
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
         }
+
+        execvp(argv[0], const_cast<char *const *>(argv.data()));
+
+        std::string msg = "failed to execute command";
+
+        if (errno == ENOENT)
+            msg = "command not found";
+
+        std::cerr << tokens[0] << ": " << msg << std::endl;
+        exit(1);
     }
-    else // parent process (pid > 0)
-        waitpid(pid, nullptr, 0);
+
+    else
+    {
+        if (!background)
+            waitpid(pid, nullptr, 0);
+        else
+            std::cout << "[background pid " << pid << "]" << std::endl;
+    }
 }
