21b76ce0406f1ada63fb7b4a6b3907924de63e5c
youssef <youssefalfred3012006@gmail.com>
2026-03-14 17:34:39 +0200

    hey Dr. , this is y. Gohar , 900246169 , here is the submission. hoope u like it :D

---
+++ src/CMakeFiles/nsh.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nsh.dir/builtins.cpp.o"
+  "CMakeFiles/nsh.dir/builtins.cpp.o.d"
+  "CMakeFiles/nsh.dir/executer.cpp.o"
+  "CMakeFiles/nsh.dir/executer.cpp.o.d"
+  "CMakeFiles/nsh.dir/main.cpp.o"
+  "CMakeFiles/nsh.dir/main.cpp.o.d"
+  "CMakeFiles/nsh.dir/parser.cpp.o"
+  "CMakeFiles/nsh.dir/parser.cpp.o.d"
+  "nsh"
+  "nsh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nsh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/CMakeFiles/nsh.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for nsh.
+# This may be replaced when dependencies are built.
