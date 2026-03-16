20fb1481b94a326929515d7dae01aab17b36de49
Youssef Micheal <YoussefMicheal@aucegypt.edu>
2026-03-16 20:35:21 +0200

    Fix output redirection and background process implementation

--- src/executer.cpp
+++ src/executer.cpp
@@ -4,49 +4,57 @@
 #include "sys/wait.h"
 #include <iostream>
 #include <vector>
+#include <string>
+#include "fcntl.h"
+#include <cerrno>
 
 void Executer::execute(const std::vector<std::string> &tokens)
 {
-
-int redirectIndex = -1;
-
-for(int i = 0; args[i] != NULL; i++){
-	 if(string(args[i]) == ">"){
-	 redirectIndex = i;
-	 break;
-	 }
-}
-
-if(redirectIndex != -1){
-	 int fd = open(args[redirectIndex + 1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-
-	 dup2(fd, STDOUT_FILENO);
-	 close(fd);
-
-	 args[redirectIndex] = NULL;
-}
-
-bool background = false;
-
-for(int i = 0; args[i] != NULL; i++){
-	 if(args[i+1] == NULL && string(args[i]) == "&"){
-	 background = true;
-	 args[i] = NULL;
-	 }
-}
     if (Builtins::handle(tokens))
         return;
 
+    std::vector<std::string> args = tokens;
+
+    int redirectIndex = -1;
+
+    for (int i = 0; i < args.size(); i++)
+    {
+        if (args[i] == ">")
+        {
+            redirectIndex = i;
+            break;
+        }
+    }
+
+    bool background = false;
+
+    if (!args.empty() && args.back() == "&")
+    {
+        background = true;
+        args.pop_back();
+    }
+
     std::vector<const char *> argv;
 
-    for (const std::string &token : tokens)
-        argv.push_back(token.c_str());
+    for (const std::string &arg : args)
+        argv.push_back(arg.c_str());
+
     argv.push_back(nullptr);
 
     pid_t pid = fork();
 
     if (pid == 0)
     {
+        if (redirectIndex != -1)
+        {
+            int fd = open(args[redirectIndex + 1].c_str(), O_CREAT | O_WRONLY | O_TRUNC, 0644);
+
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+
+            argv[redirectIndex] = nullptr;
+        }
+
         int status = execvp(argv[0], const_cast<char *const *>(argv.data()));
 
         if (status != 0)
@@ -62,5 +70,8 @@ for(int i = 0; args[i] != NULL; i++){
     else if (pid == -1)
         std::cerr << tokens[0] << ": failed to execute command" << std::endl;
     else
-        waitpid(pid, nullptr, 0);
+    {
+        if (!background)
+            waitpid(pid, nullptr, 0);
+    }
 }
