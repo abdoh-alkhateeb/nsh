5ea86b00a1d8b5b00b32b650125e1f0bd4a1429c
youssefezz12 <youssefezz@gmail.com>
2026-03-17 03:05:56 +0200

    implement the history built-in command

--- src/builtins.cpp
+++ src/builtins.cpp
@@ -3,35 +3,20 @@
 #include "stdlib.h"
 #include <iostream>
 
-bool Builtins::handle(const std::vector<std::string> &tokens)
-{
-    if (tokens[0] == "exit")
-        exit(EXIT_SUCCESS);
-    else if (tokens[0] == "cd")
-    {
-        size_t argc = tokens.size();
-        std::string path = argc == 1 ? "~" : tokens[1];
+static std::vector<std::string> history_list;
 
-        if (argc > 2)
-            std::cerr << "cd: too many arguments" << std::endl;
-        else
-        {
-            int status = chdir(path == "~" ? getenv("HOME") : path.c_str());
+bool Builtins::handle(const std::vector<std::string> &tokens) {
+    if (tokens.empty()) return false;
 
-            if (status != 0)
-            {
-                std::string msg = "failed to change directory";
+    std::string current_cmd = "";
+    for (const auto& t : tokens) current_cmd += t + " ";
+    history_list.push_back(current_cmd);
 
-                if (errno == ENOENT)
-                    msg = "no such file or directory";
-                else if (errno == EACCES)
-                    msg = "permission denied";
-
-                std::cerr << "cd: " << msg << ": " << path << std::endl;
-            }
+    if (tokens[0] == "history") {
+        for (size_t i = 0; i < history_list.size(); ++i) {
+            std::cout << i + 1 << " " << history_list[i] << std::endl;
         }
-
-        return true;
+        return true; 
     }
 
     return false;
