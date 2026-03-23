dc1a34d87a42728ad2640655119f4ed25f718143
jessica-basem <jessicabasem@aucegypt.edu>
2026-03-23 17:27:30 +0000

    assignment 1 jessica

--- src/builtins.cpp
+++ src/builtins.cpp
@@ -1,35 +1,38 @@
 #include "builtins.hpp"
-#include "unistd.h"
-#include "stdlib.h"
 #include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <vector>
+
+static std::vector<std::string> history;
 
 bool Builtins::handle(const std::vector<std::string> &tokens)
 {
+    if (tokens.empty())
+        return true;
+
+    history.push_back(tokens[0]);
+
     if (tokens[0] == "exit")
         exit(EXIT_SUCCESS);
+
+    else if (tokens[0] == "history")
+    {
+        for (size_t i = 0; i < history.size(); i++)
+            std::cout << i + 1 << " "
+                      << history[i] << std::endl;
+
+        return true;
+    }
+
     else if (tokens[0] == "cd")
     {
-        size_t argc = tokens.size();
-        std::string path = argc == 1 ? "~" : tokens[1];
+        std::string path =
+            tokens.size() == 1 ? getenv("HOME")
+                               : tokens[1];
 
-        if (argc > 2)
-            std::cerr << "cd: too many arguments" << std::endl;
-        else
-        {
-            int status = chdir(path == "~" ? getenv("HOME") : path.c_str());
-
-            if (status != 0)
-            {
-                std::string msg = "failed to change directory";
-
-                if (errno == ENOENT)
-                    msg = "no such file or directory";
-                else if (errno == EACCES)
-                    msg = "permission denied";
-
-                std::cerr << "cd: " << msg << ": " << path << std::endl;
-            }
-        }
+        if (chdir(path.c_str()) != 0)
+            std::cerr << "cd failed\n";
 
         return true;
     }
--- src/executer.cpp
+++ src/executer.cpp
@@ -1,39 +1,78 @@
 #include "executer.hpp"
 #include "builtins.hpp"
-#include "unistd.h"
-#include "sys/wait.h"
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 #include <iostream>
 #include <vector>
 
 void Executer::execute(const std::vector<std::string> &tokens)
 {
+    if (tokens.empty())
+        return;
+
     if (Builtins::handle(tokens))
         return;
 
+    std::vector<std::string> args = tokens;
+
+    bool background = false;
+
+    if (args.back() == "&")
+    {
+        background = true;
+        args.pop_back();
+    }
+
+    int redirectIndex = -1;
+
+    for (size_t i = 0; i < args.size(); i++)
+    {
+        if (args[i] == ">")
+        {
+            redirectIndex = i;
+            break;
+        }
+    }
+
+    int fd = -1;
+
+    if (redirectIndex != -1)
+    {
+        std::string filename = args[redirectIndex + 1];
+
+        fd = open(filename.c_str(),
+                  O_WRONLY | O_CREAT | O_TRUNC,
+                  0644);
+
+        args.resize(redirectIndex);
+    }
+
     std::vector<const char *> argv;
 
-    for (const std::string &token : tokens)
-        argv.push_back(token.c_str());
+    for (const std::string &s : args)
+        argv.push_back(s.c_str());
+
     argv.push_back(nullptr);
 
     pid_t pid = fork();
 
-    if (pid < 0) // fork failed
-        std::cerr << tokens[0] << ": failed to execute command" << std::endl;
-    else if (pid == 0) // child process
+    if (pid == 0)
     {
-        int status = execvp(argv[0], const_cast<char *const *>(argv.data()));
-
-        if (status != 0)
+        if (fd != -1)
         {
-            std::string msg = "failed to execute command";
-
-            if (errno == ENOENT)
-                msg = "command not found";
-
-            std::cerr << tokens[0] << ": " << msg << std::endl;
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
         }
+
+        execvp(argv[0],
+               const_cast<char *const *>(argv.data()));
+
+        std::cerr << "command failed\n";
     }
-    else // parent process (pid > 0)
-        waitpid(pid, nullptr, 0);
+    else
+    {
+        if (!background)
+            waitpid(pid, nullptr, 0);
+    }
 }
